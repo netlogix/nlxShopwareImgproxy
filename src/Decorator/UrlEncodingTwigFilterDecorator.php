cd0d86afafee13156a9b01465bbc6665e8d9e95a
vladislav.sultanov <vladislav.sultanov@netlogix.de>
2025-09-30 10:29:04 +0200

    FEAT: Override shopware function to prevent encoding imgproxy url
    Without this change values size values like "width:240" are encoded to "width%3A240"

---
+++ src/Decorator/UrlEncodingTwigFilterDecorator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Netlogix\NlxSwImgproxy\Decorator;
+
+use Netlogix\NlxSwImgproxy\Service\ConfigService;
+use Shopware\Storefront\Framework\Twig\Extension\UrlEncodingTwigFilter;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+
+#[AsDecorator(decorates: UrlEncodingTwigFilter::class)]
+class UrlEncodingTwigFilterDecorator extends UrlEncodingTwigFilter
+{
+    public function __construct(
+        private readonly ConfigService $configService
+    ) {
+    }
+
+    public function encodeUrl(?string $mediaUrl): ?string
+    {
+        if ($this->shouldReturnMediaUrl($mediaUrl)) {
+            return $mediaUrl;
+        }
+
+        return parent::encodeUrl($mediaUrl);
+    }
+
+    private function shouldReturnMediaUrl(?string $mediaUrl): bool
+    {
+        if ($mediaUrl === null) {
+            return false;
+        }
+
+        if (!$this->configService->isEnabled()) {
+            return false;
+        }
+
+        $baseUrl = $this->configService->getBaseUrl();
+
+        if (empty($baseUrl)) {
+            return false;
+        }
+
+        return str_contains($mediaUrl, $baseUrl);
+    }
+}
