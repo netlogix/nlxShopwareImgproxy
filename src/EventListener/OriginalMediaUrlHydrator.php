5ecf5ab83abaf849af417984c4f5029a7e1c92dc
Julius Noack <julius.noack@netlogix.de>
2026-03-05 15:20:14 +0100

    refactore: move bypass method to only call it once

--- src/EventListener/OriginalMediaUrlHydrator.php
+++ src/EventListener/OriginalMediaUrlHydrator.php
@@ -6,12 +6,9 @@ use Netlogix\NlxSwImgproxy\Core\Content\Entity\OriginalMediaEntity;
 use Netlogix\NlxSwImgproxy\Decorator\ImgProxyMediaUrlGenerator;
 use Shopware\Core\Content\Media\Core\Application\AbstractMediaUrlGenerator;
 use Shopware\Core\Content\Media\Core\Params\UrlParams;
-use Shopware\Core\Content\Media\Infrastructure\Path\MediaUrlGenerator;
 use Shopware\Core\Content\Media\MediaEntity;
 use Shopware\Core\Content\Media\MediaEvents;
-use Shopware\Core\Content\Product\ProductEvents;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityLoadedEvent;
-use Shopware\Storefront\Page\Product\ProductPageCriteriaEvent;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 #[AsEventListener(MediaEvents::MEDIA_LOADED_EVENT)]
@@ -24,20 +21,20 @@ readonly class OriginalMediaUrlHydrator
 
     public function __invoke(EntityLoadedEvent $event): void
     {
-        /** @var MediaEntity $mediaEntity */
-        foreach ($event->getEntities() as $mediaEntity) {
-            $url = '';
-            if ($mediaEntity->hasPath()) {
-                ImgProxyMediaUrlGenerator::bypass(
-                    function () use (&$url, $mediaEntity) {
+        ImgProxyMediaUrlGenerator::bypass(
+            function () use ($event) {
+                /** @var MediaEntity $mediaEntity */
+                foreach ($event->getEntities() as $mediaEntity) {
+                    $url = '';
+                    if ($mediaEntity->hasPath()) {
                         $url = $this->mediaUrlGenerator->generate([UrlParams::fromMedia($mediaEntity)]);
                     }
-                );
-            }
 
-            $mediaEntity->addArrayExtension('originalMedia', [
-                'originalUrl' => $url,
-            ]);
-        }
+                    $mediaEntity->addArrayExtension('originalMedia', [
+                        'originalUrl' => $url,
+                    ]);
+                }
+            }
+        );
     }
 }
