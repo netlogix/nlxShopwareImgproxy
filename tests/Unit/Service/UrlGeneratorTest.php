55661e6b0625c8a423f23997da1eeb8d77bcc8fa
TheBreaken <22234380+TheBreaken@users.noreply.github.com>
2026-06-25 12:36:33 +0000

    [automated] Apply Coding Standard

--- tests/Unit/Service/UrlGeneratorTest.php
+++ tests/Unit/Service/UrlGeneratorTest.php
@@ -49,22 +49,22 @@ class UrlGeneratorTest extends TestCase
 
         yield 'secure' => [
             'imagePath' => 'test/image.jpg',
-            'key' => '16901a13d9b2f42d312122e2069f696c3c32fa9bf130002c8e902e1f09a9b5298ae28bd674272037e94a10e21000fa9748c32434dd2c9ccc32aa1c9ee897138e',
-            'salt' => '0c603978ab5316d93df3a3ece0d76e3a7e8a81a0000c1820563f5274dad799e7faffeb8cc95bc999c5c1a000240ec1d89918a21139751bf808f5f77ea4134a47',
-            'expectedUrl' => '#^https://image-proxy\.example\.com/([A-Za-z0-9_-]{43})/([a-zA-Z0-9_-]+)$#i',
+            'expectedUrl' => '16901a13d9b2f42d312122e2069f696c3c32fa9bf130002c8e902e1f09a9b5298ae28bd674272037e94a10e21000fa9748c32434dd2c9ccc32aa1c9ee897138e',
+            'width' => '0c603978ab5316d93df3a3ece0d76e3a7e8a81a0000c1820563f5274dad799e7faffeb8cc95bc999c5c1a000240ec1d89918a21139751bf808f5f77ea4134a47',
+            'height' => '#^https://image-proxy\.example\.com/([A-Za-z0-9_-]{43})/([a-zA-Z0-9_-]+)$#i',
         ];
 
         yield 'with resizeType' => [
             'imagePath' => 'test/image.jpg',
             'expectedUrl' => '#^https://image-proxy\.example\.com/insecure/rt:fill-down/[a-zA-Z0-9_-]+$#i',
-            'resizeType' => ResizeType::fillDown
+            'width' => ResizeType::fillDown
         ];
 
         yield 'with size' => [
             'imagePath' => 'test/image.jpg',
-            'width' => '100',
-            'height' => '200',
-            'expectedUrl' => '#^https://image-proxy\.example\.com/insecure/w:100/h:200/[a-zA-Z0-9_-]+$#i',
+            'expectedUrl' => '100',
+            'width' => '200',
+            'height' => '#^https://image-proxy\.example\.com/insecure/w:100/h:200/[a-zA-Z0-9_-]+$#i',
         ];
 
         yield 'with cacheBuster' => [
